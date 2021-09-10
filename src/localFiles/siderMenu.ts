59c43bc92ea92d6d0132cbd1006950848f0ed4f3
chenweiyan <chanweiyan007@gmail.com>
2021-09-10 20:51:21 +0800

    docs: NavLink & Prompt

--- src/localFiles/siderMenu.ts
+++ src/localFiles/siderMenu.ts
@@ -1,4 +1,5 @@
 export const siderMenuLinks = [
+  { path: '/', name: 'home' },
   { path: '/useToggle', name: 'useToggle' },
   { path: '/chatUI', name: 'chatUI' },
 ];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
+import { Row, Col } from 'antd';
+import { NavLink, Prompt, useRouteMatch } from 'umi';
 import styles from './index.less';
 
 export default function IndexPage() {
+  const match = useRouteMatch();
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      {/* <Prompt message="你确定要离开么？" /> */}
+      {/* 根据一个状态来确定用户离开页面时是否给一个提示选择 */}
+      {/* <Prompt when={formIsHalfFilledOut} message="您确定半途而废么？" /> */}
+      <ul>
+        <li>match: {JSON.stringify(match)}</li>
+      </ul>
+      <Row>
+        <Col span={6}>
+          <NavLink to="/useToggle">useToggle</NavLink>
+        </Col>
+        <Col span={6}>
+          <NavLink to="/chatUI">chatUI</NavLink>
+        </Col>
+      </Row>
     </div>
   );
 }
