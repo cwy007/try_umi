315d53001e40081ea8b2e665a25d49af71f0a55f
chenweiyan <chanweiyan007@gmail.com>
2021-09-11 01:34:51 +0800

    docs: rowSelection

--- src/pages/table/rowSelection.tsx
+++ src/pages/table/rowSelection.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { Tabs } from 'antd';
+import BasicRowSelection from './components/BasicRowSelection';
+import SpecialRowSelection from './components/SpecialRowSelection';
 
-const RowSelection = () => {
-  return <div></div>;
-};
+const { TabPane } = Tabs;
 
-export default RowSelection;
+function callback(key) {
+  console.log(key);
+}
+
+const Demo = () => (
+  <Tabs defaultActiveKey="1" onChange={callback}>
+    <TabPane tab="BasicRowSelection" key="1">
+      <BasicRowSelection />
+    </TabPane>
+    <TabPane tab="SpecialRowSelection" key="2">
+      <SpecialRowSelection />
+    </TabPane>
+  </Tabs>
+);
+
+export default Demo;
