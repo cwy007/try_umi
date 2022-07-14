1c3a96ef4b05e7da2e1a166858e16a60b6786b58
chenweiyan <chanweiyan007@gmail.com>
2022-07-14 23:52:18 +0800

    fix: first try ag-grid

---
+++ src/pages/agGrid/simpleCellRender/index.tsx
@@ -0,0 +1,54 @@
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+// import MedalCellRenderer from './components/MedalCellRenderer.jsx';
+// import TotalValueRenderer from './components/TotalValueRenderer.jsx';
+
+const GridExample = () => {
+  const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
+  const gridStyle = useMemo(() => ({ height: '10000%', width: '100%' }), []);
+  const [rowData, setRowData] = useState();
+  const [columnDefs, setColumnDefs] = useState([
+    { field: 'athlete' },
+    { field: 'year' },
+    // { field: 'gold', cellRenderer: MedalCellRenderer },
+    // { field: 'silver', cellRenderer: MedalCellRenderer },
+    // { field: 'bronze', cellRenderer: MedalCellRenderer },
+    // { field: 'total', minWidth: 175, cellRenderer: TotalValueRenderer },
+  ]);
+  const defaultColDef = useMemo(() => {
+    return {
+      editable: true,
+      sortable: true,
+      flex: 1,
+      minWidth: 100,
+      filter: true,
+      resizable: true,
+    };
+  }, []);
+
+  const onGridReady = useCallback((params) => {
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .then((resp) => resp.json())
+      .then((data) => {
+        setRowData(data);
+      });
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <div style={gridStyle} className="ag-theme-alpine">
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          onGridReady={onGridReady}
+        ></AgGridReact>
+      </div>
+    </div>
+  );
+};
+
+export default GridExample;
+// render(<GridExample></GridExample>, document.querySelector('#root'));
